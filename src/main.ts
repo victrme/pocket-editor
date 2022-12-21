8476292da34bbd458914bf1c8c798b13c785dcc0
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-12-21 11:38:41 +0100

    Fixed file oopsie, started text decoration

--- src/main.ts
+++ src/main.ts
@@ -13,15 +13,21 @@ function generateLine(target?: HTMLElement) {
 	wrapper.appendChild(content)
 
 	// Find where to put the new line
-	if (target?.nextElementSibling) container?.insertBefore(wrapper, target.nextElementSibling)
+	const parentSibling = target?.parentElement?.nextElementSibling
+	if (parentSibling) container?.insertBefore(wrapper, parentSibling)
 	else container?.appendChild(wrapper)
 
 	// Does it need transformation ?
-	if (target?.parentElement?.classList.contains("todo-list")) {
-		transformToTodolist(content)
-	}
+	if (target?.parentElement?.classList.contains("todo-list")) transformToTodolist(content)
+	if (target?.parentElement?.classList.contains("unordered-list")) transformToUnorderedList(content)
 
 	content.focus()
+
+	// for debug
+	if (!target) {
+		content.textContent =
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus, nunc ut faucibus placerat, mauris quam vehicula mauris, in volutpat risus diam in lectus. Aenean ultricies risus sit amet risus fermentum ullamcorper. Nulla tincidunt laoreet lorem non fermentum. Nulla libero quam, suscipit imperdiet orci non, malesuada auctor massa. Sed faucibus nulla vel nibh faucibus, vitae imperdiet nisl auctor. Etiam sit amet mi tincidunt, euismod velit a, mattis quam. Aliquam aliquet lacinia aliquam. Nunc sed nisi sed sapien fermentum pulvinar ac vel elit. Sed sed aliquam sapien, at ullamcorper metus. Donec finibus ante ut urna porttitor, et tincidunt augue sagittis. Curabitur nulla odio, tincidunt ut felis et, efficitur eleifend ex. "
+	}
 }
 
 function removeLine(target: Element) {
@@ -85,6 +91,10 @@ function transformToTodolist(target: HTMLElement) {
 	if (!parent) return
 
 	input.type = "checkbox"
+	input.addEventListener("input", () => {
+		if (input.checked) input.setAttribute("checked", "")
+		else input.removeAttribute("checked")
+	})
 
 	parent?.classList.add("modif-line")
 	parent?.classList.add("todo-list")
@@ -94,13 +104,115 @@ function transformToTodolist(target: HTMLElement) {
 	target.focus()
 }
 
+function transformToUnorderedList(target: HTMLElement) {
+	const span = document.createElement("span")
+	const parent = target.parentElement
+
+	if (!parent) return
+
+	span.dataset.content = "•"
+	span.classList.add("list-dot")
+
+	parent?.classList.add("modif-line")
+	parent?.classList.add("unordered-list")
+	parent.prepend(span)
+
+	target.innerHTML = target.innerHTML.replace("-", "")
+	target.focus()
+}
+
+function textStylingControl(range: Range, e: KeyboardEvent) {
+	const selectionLen = range.endOffset - range.startOffset
+	if (selectionLen === 0) return
+
+	function splitTextNodeAsSpan(style: string) {
+		const target = e.target as HTMLDivElement
+		const editableText = target.innerText || ""
+
+		console.log(target)
+
+		const pre = document.createTextNode(editableText.slice(0, range.startOffset))
+		const post = document.createTextNode(editableText.slice(range.endOffset, editableText.length - 1))
+		const span = document.createElement("span")
+
+		span.textContent = editableText.slice(range.startOffset, range.endOffset)
+
+		if (style === "italics") {
+			span.className = "text-italics"
+		}
+
+		// type TextModifiers = {
+		// 	val: string
+		// 	pos: [number, number]
+		// 	bold?: true
+		// 	italics?: true
+		// 	strike?: true
+		// 	code?: true
+		// }
+
+		// let nodeStartPos = 0
+		// let currentEditableText: TextModifiers[] = []
+
+		// Object.values(target?.childNodes).forEach((node) => {
+		// 	const val = node.nodeValue || ""
+		// 	const pos: [number, number] = [nodeStartPos, nodeStartPos + val.length - 1]
+		// 	let res: TextModifiers = { pos, val }
+
+		// 	// detect and add stylings to node object
+		// 	if (node.nodeName === "SPAN") {
+		// 		if ((node as Element).className === "text-italics") res.italics = true
+		// 		if ((node as Element).className === "text-bold") res.bold = true
+		// 		if ((node as Element).className === "text-strike") res.strike = true
+		// 		if ((node as Element).className === "text-code") res.code = true
+		// 	}
+
+		// 	// next node will start at the end of this one
+		// 	nodeStartPos += val.length
+		// 	currentEditableText.push(res)
+		// })
+
+		// suppr tout
+		target.innerHTML = ""
+		target.appendChild(pre)
+		target.appendChild(span)
+		target.appendChild(post)
+		target.focus()
+	}
+
+	if (e.key === "i" && e.ctrlKey) {
+		e.preventDefault()
+		console.log("style selection to italics")
+		splitTextNodeAsSpan("italics")
+	}
+
+	if (e.key === "b" && e.ctrlKey) {
+		e.preventDefault()
+		console.log("style selection to bold")
+	}
+
+	if (e.key === "s" && e.ctrlKey) {
+		e.preventDefault()
+		console.log("style selection to strike")
+	}
+
+	if (e.key === "e" && e.ctrlKey) {
+		e.preventDefault()
+		console.log("style selection to code")
+	}
+}
+
 function lineKeyboardEvent(e: Event) {
 	const container = document.querySelector("#container")
 	const range = window.getSelection()?.getRangeAt(0)
 	const target = e.target as HTMLDivElement
 
 	if (!range || !target || !container) return
-	console.log(e)
+
+	//
+	//
+	textStylingControl(range, e as KeyboardEvent)
+	//
+	//
 
 	if ((e as KeyboardEvent).key === "Enter" && (e as KeyboardEvent).shiftKey === false) {
 		e.preventDefault()
@@ -147,16 +259,22 @@ function lineKeyboardEvent(e: Event) {
 	}
 
 	if ((e as KeyboardEvent).key === " " && target.textContent?.startsWith("-") && range.endOffset === 1) {
-		console.log("modify to unordered list element")
+		e.preventDefault()
+		transformToUnorderedList(target)
 	}
 
-	if ((e as KeyboardEvent).key === " " && target.textContent?.startsWith("1.") && range.endOffset === 1) {
-		console.log("modify to ordered element")
-	}
+	// if ((e as KeyboardEvent).key === " " && target.textContent?.startsWith("1.") && range.endOffset === 1) {
+	// 	console.log("modify to ordered element")
+	// }
 
-	if ((e as KeyboardEvent).key === "-" && target.textContent?.startsWith("--") && range.endOffset === 2) {
-		console.log("modify to separator")
-	}
+	// if ((e as KeyboardEvent).key === "-" && target.textContent?.startsWith("--") && range.endOffset === 2) {
+	// 	console.log("modify to separator")
+	// }
+
+	// Set "prepare text transform" value when inputing a "*", "**", "~~"
+	// If it finds another similar input, split text node to add transform tag
+	// set a "transformed text" value to true
+	// If user hits backspace, removes transform
 }
 
 window.addEventListener("load", function () {
