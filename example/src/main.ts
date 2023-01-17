245c93a90c164a69afde5bf4bb8273786f7909c2
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-01-17 15:11:42 +0100

    Added "log content to console" to example

--- example/src/main.ts
+++ example/src/main.ts
@@ -6,4 +6,8 @@ import "./style.css"
 window.addEventListener("load", function () {
 	const editor = tinyNotes("wrapper")
 	editor.set(markdown)
+
+	this.document.querySelector("#b_logcontent")?.addEventListener("click", function () {
+		console.log(editor.get())
+	})
 })
