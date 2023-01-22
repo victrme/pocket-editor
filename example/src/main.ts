e721a858285da962f24219d753c72b3e587b25c8
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-01-22 11:39:02 +0100

    Removed dep for debugging convenience

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,13 +1,10 @@
-import pocketEditor from "pocket-editor"
+import pocketEditor from "../../src/index"
+import "../../src/style.css"
+
 import markdown from "./markdown"
-import "pocket-editor/dist/style.css"
 import "./style.css"
 
 window.addEventListener("load", function () {
 	const editor = pocketEditor("wrapper")
 	editor.set(markdown)
-
-	this.document.querySelector("#b_logcontent")?.addEventListener("click", function () {
-		console.log(editor.get())
-	})
 })
