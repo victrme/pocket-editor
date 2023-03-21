405b4924b816e134003632edf01fb601aad802ba
Victor Azevedo <victor.azevedo@mail.com>
2023-03-21 12:12:28 +0100

    Improved get, set & line transform tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test, beforeEach } from "@jest/globals"
+import { describe, expect, test, beforeEach, beforeAll } from "@jest/globals"
 import pocketEditor from "./index"
 
 let editor = pocketEditor("")
@@ -16,20 +16,35 @@ describe("Is working", () => {
 	test("Appends to wrapper", () => {
 		expect(document.getElementById("pocket-editor")?.id).toEqual("pocket-editor")
 	})
+})
 
-	test("Sets text", async () => {
-		const text = "Hello world"
+describe("Set", () => {
+	beforeEach(() => {
+		const text = "Hello world\n\nHello !\n\nHello\tworld"
 		editor.set(text)
-		expect(document.getElementById("pocket-editor")?.textContent).toEqual(text)
 	})
 
-	test("Sets multiple lines", () => {
-		const text = `Hello\nworld`
-		editor.set(text)
-		expect(document.querySelectorAll("#pocket-editor .line")?.length).toEqual(2)
+	test("Never empty", () => {
+		editor.set("")
+		expect(!!document.querySelector("[contenteditable]")).toBe(true)
+		expect(document.querySelector("[contenteditable]")?.textContent).toEqual("")
 	})
 
-	test("Gets text", () => {
+	test("Single line", () => {
+		expect(document.querySelector(".line")?.textContent).toEqual("Hello world")
+	})
+
+	test("Multiple lines", () => {
+		expect(document.querySelectorAll("#pocket-editor .line")?.length).toEqual(3)
+	})
+
+	test("Removes tabs", () => {
+		expect(document.querySelectorAll(".line")[2]?.textContent).toEqual("Helloworld")
+	})
+})
+
+describe("Get", () => {
+	test("Single line", () => {
 		const text = "Hello world"
 		editor.set(text)
 		expect(editor.get()).toEqual(text)
@@ -37,25 +52,39 @@ describe("Is working", () => {
 })
 
 describe("Line transform", () => {
-	const text = `Hello world\n\n# h1\n\n## h2\n\n### h3\n\n- ul\n\n- ul\n\n[ ] c\n\n[x] cc`
-
-	editor.set(text)
+	beforeEach(() => {
+		editor.set(`Hello world\n\n# h1\n\n## h2\n\n### h3\n\n- ul\n\n- ul\n\n[ ] c\n\n[x] cc`)
+	})
 
 	test("Has big heading", () => {
-		expect(!!document.querySelector("h1")).toBe(true)
+		const title = document.querySelector("h1[contenteditable]")
+		expect(title?.textContent).toEqual("h1")
+		expect(title?.parentElement?.className).toEqual("line mod h1")
 	})
+
 	test("Has medium heading", () => {
-		expect(!!document.querySelector("h2")).toBe(true)
+		const title = document.querySelector("h2[contenteditable]")
+		expect(title?.textContent).toEqual("h2")
+		expect(title?.parentElement?.className).toEqual("line mod h2")
 	})
+
 	test("Has small heading", () => {
-		expect(!!document.querySelector("h3")).toBe(true)
+		const title = document.querySelector("h3[contenteditable]")
+		expect(title?.textContent).toEqual("h3")
+		expect(title?.parentElement?.className).toEqual("line mod h3")
 	})
+
 	test("Has unordered list", () => {
-		expect(!!document.querySelector("span.list-dot")).toBe(true)
+		const line = document.querySelector(".line.mod.ul-list")
+
+		expect(!!line?.querySelector("span.list-dot")).toBe(true)
+		expect(line?.querySelector("div[contenteditable]")?.textContent).toEqual("ul")
 	})
+
 	test("Has checkbox", () => {
 		expect(!!document.querySelector("input[type='checkbox']")).toBe(true)
 	})
+
 	test("Has checked checkbox", () => {
 		expect(!!document.querySelector("input[type='checkbox']:checked")).toBe(true)
 	})
