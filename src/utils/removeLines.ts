ae4cebe1209b46fd6b7879377eebd249f16a288a
Victor Azevedo <victor.azevedo@mail.com>
2023-03-25 10:33:11 +0100

    Stronger line focus, cleaner code

--- src/utils/removeLines.ts
+++ src/utils/removeLines.ts
@@ -1,31 +1,24 @@
 import generateLine from "../lib/lineGenerate"
+import setCaret from "./setCaret"
 
 function insertAfter(newNode: Node, existingNode: Node) {
 	existingNode?.parentNode?.insertBefore(newNode, existingNode.nextSibling)
-}
-
-function focusOnEditable(line: Element | undefined) {
-	line?.querySelector<HTMLElement>("[contenteditable]")?.focus()
 }
 
 export default function removeLines(lines: Element[], container: Element) {
 	const prevLine = lines[0].previousElementSibling
 	const emptyLine = generateLine()
 
-	// remove selected lines
+	// remove
 	lines.forEach((line) => line.remove())
 
-	// focus on last line
-	if (prevLine) {
-		insertAfter(emptyLine, prevLine)
-		focusOnEditable(emptyLine)
-	} else {
-		// no prev line, create one
-		container.prepend(emptyLine)
-		focusOnEditable(emptyLine)
-	}
+	// insert
+	prevLine ? insertAfter(emptyLine, prevLine) : container.prepend(emptyLine)
 
-	// Create a mock event to trigger oninput
+	// focus
+	setCaret(emptyLine?.childNodes[0])
+
+	// Mock event to trigger oninput
 	container.dispatchEvent(
 		new InputEvent("input", {
 			inputType: "deleteContent",
