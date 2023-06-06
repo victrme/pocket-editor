1313ba2382f564a1775ca60b0ccf56121bd2c8c0
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-06-06 11:08:05 +0200

    Forgot to remove console.log

--- src/utils/getSelectedLines.ts
+++ src/utils/getSelectedLines.ts
@@ -1,8 +1,4 @@
 export default function getSelectedLines(container?: HTMLElement): HTMLElement[] {
 	container = container ?? (document.getElementById("pocket-editor") as HTMLElement)
-	const res = Object.values(container?.querySelectorAll<HTMLElement>(".line.sel") ?? [])
-
-	console.log(res)
-
-	return res
+	return Object.values(container?.querySelectorAll<HTMLElement>(".line.sel") ?? [])
 }
