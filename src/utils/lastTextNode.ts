164c650e8e19d04d3c7794e26a747ad7edfb7055
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-06-19 10:57:03 +0200

    Fixed infinite loop when removing whole lines

--- src/utils/lastTextNode.ts
+++ src/utils/lastTextNode.ts
@@ -1,16 +1,14 @@
 export default function lastTextNode(line: Node) {
 	let lastNode = line
 
-	while (lastNode?.childNodes) {
-		const childNodes = Object.values(lastNode.childNodes)
+	while (lastNode?.childNodes.length > 0) {
+		const childNodes = Object.values(lastNode.childNodes).reverse()
 		const textNodes = childNodes.filter((child) => child.nodeName === "#text")
 
 		if (textNodes.length > 0) {
-			return textNodes.reverse()[0]
-		}
-
-		if (childNodes.length > 0) {
-			lastNode = childNodes[childNodes.length - 1]
+			return textNodes[0]
+		} else {
+			lastNode = childNodes[0]
 		}
 	}
 
