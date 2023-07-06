71ba06922f1ad45a76b2e1e1c93f11029b382ccf
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2023-07-06 15:47:30 +0200

    Added basic working tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,11 +20,11 @@ export default defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: "html",
+	reporter: "line",
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		// baseURL: 'http://127.0.0.1:3000',
+		// baseURL: "http://127.0.0.1:5173",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
@@ -70,8 +70,9 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
+	// 	command: "pnpm dev",
+	// 	url: "http://localhost:5173",
+	// 	timeout: 120 * 1000,
+	// 	reuseExistingServer: !process.env.CI,
 	// },
 })
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,62 @@
 import { test, expect } from "@playwright/test"
 
-test("has title", async ({ page }) => {
-	await page.goto("localhost:4173")
-	await expect(page).toHaveTitle("Pocket editor example")
+test.describe("Contenteditable Text Editor", () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto("http://localhost:4173")
+	})
+
+	test.afterAll(async ({ page }) => {
+		await page.close()
+	})
+
+	test("Pocket editor exists", async ({ page }) => {
+		const dom = page.locator("#pocket-editor")
+		expect(dom).toBeTruthy()
+	})
+
+	test("should move the cursor", async ({ page }) => {
+		await page.click("#pocket-editor") // Replace with your editor's selector
+
+		await page.keyboard.press("ArrowRight")
+		await page.keyboard.press("ArrowRight")
+
+		const cursorPosition = await page.evaluate(() => {
+			const selection = window.getSelection()
+			return selection?.getRangeAt(0).startOffset
+		})
+
+		expect(cursorPosition).toBe(2)
+	})
+
+	test("should add a new line", async ({ page }) => {
+		await page.locator("#pocket-editor [contenteditable]").first().focus()
+
+		const oldlines = await page.$$eval(".line", (lines) => lines.length)
+		await page.keyboard.press("ArrowRight")
+		await page.keyboard.press("Enter")
+		const newlines = await page.$$eval(".line", (lines) => lines.length)
+
+		expect(newlines).toBe(oldlines + 1)
+	})
+
+	test("should remove a line", async ({ page }) => {
+		const oldlines = await page.$$eval(".line", (lines) => lines.length)
+
+		await page.locator("#pocket-editor [contenteditable]").nth(1).focus()
+		await page.keyboard.press("Backspace")
+
+		const newlines = await page.$$eval(".line", (lines) => lines.length)
+
+		expect(newlines).toBe(oldlines - 1)
+	})
+
+	test("backspace removes a transform", async ({ page }) => {
+		const element = page.locator("#pocket-editor [contenteditable]").first()
+		const line = page.locator("#pocket-editor .line").first()
+
+		await element.focus()
+		await page.keyboard.press("Backspace")
+
+		await expect(line).not.toHaveClass("h2")
+	})
 })
