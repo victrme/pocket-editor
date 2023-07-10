4544c2307658e9e99d7a5bf510011ca88cfc9130
Victor Azevedo <victor.azevedo@mail.com>
2023-07-10 08:56:52 +0200

    Added all transform behaviors in tests

--- tests/selection.test.ts
+++ tests/selection.test.ts
@@ -1,124 +1,133 @@
 import { test, expect, Locator } from "@playwright/test"
 
 let element: Locator
+let line: Locator
 let text: string
 
 test.beforeEach(async ({ page }) => {
 	await page.goto("http://localhost:4173")
 
+	line = page.locator("#pocket-editor .line").nth(1)
 	element = page.locator("#pocket-editor [contenteditable]").nth(1)
 	text = (await element.textContent()) ?? ""
 	await element.focus()
 })
 
-test("Select all text in single line", async ({ page }) => {
-	await page.keyboard.down("Shift")
+test.describe("Selection", () => {
+	test("All text in single line", async ({ page }) => {
+		await page.keyboard.down("Shift")
 
-	for (let i = 0; i < text.length; i++) {
+		for (let i = 0; i < text.length; i++) {
+			await page.keyboard.press("ArrowRight")
+		}
+
+		await page.keyboard.up("Shift")
+		await page.keyboard.press("Backspace")
+
+		expect(await element.textContent()).toBe("")
+	})
+
+	test("With shift + left", async ({ page }) => {
+		await page.keyboard.down("Shift")
+		await page.keyboard.press("ArrowLeft")
+		expect(await line.getAttribute("class")).toBe("line sel")
+	})
+
+	test("With shift + right", async ({ page }) => {
+		for (let i = 0; i < text.length; i++) {
+			await page.keyboard.press("ArrowRight")
+		}
+
+		await page.keyboard.down("Shift")
 		await page.keyboard.press("ArrowRight")
-	}
+		expect(await line.getAttribute("class")).toBe("line sel")
+	})
 
-	await page.keyboard.up("Shift")
-	await page.keyboard.press("Backspace")
+	test("With Shift + up", async ({ page }) => {
+		await page.keyboard.down("Shift")
+		await page.keyboard.press("ArrowUp")
+		expect(await line.getAttribute("class")).toBe("line sel")
+	})
 
-	expect(await element.textContent()).toBe("")
-})
-
-test("Mouse move", async ({ page }) => {
-	const box = await element.boundingBox()
-	const { x, y } = box ?? { x: 0, y: 0 }
-	await page.mouse.move(x + 50, y + 10)
-	await page.mouse.down()
-	await page.mouse.move(x + 50, y + 100)
-
-	const lines = await page.locator("#pocket-editor .line.sel").all()
-	expect(lines.length).toBe(4)
-})
-
-test("Left arrow at start", async ({ page }) => {
-	await page.keyboard.down("Shift")
-	await page.keyboard.press("ArrowLeft")
-
-	const line = page.locator("#pocket-editor .line").nth(1)
-	const cl = (await line.getAttribute("class")) ?? ""
-
-	expect(cl.includes("sel")).toBe(true)
-})
-
-test("Right arrow at end", async ({ page }) => {
-	for (let i = 0; i < text.length; i++) {
-		await page.keyboard.press("ArrowRight")
-	}
-
-	await page.keyboard.down("Shift")
-	await page.keyboard.press("ArrowRight")
-
-	const line = page.locator("#pocket-editor .line").nth(1)
-	const cl = (await line.getAttribute("class")) ?? ""
-
-	expect(cl.includes("sel")).toBe(true)
-})
-
-test("Up arrow", async ({ page }) => {
-	await page.keyboard.down("Shift")
-	await page.keyboard.press("ArrowUp")
-
-	const line = page.locator("#pocket-editor .line").nth(1)
-	const cl = (await line.getAttribute("class")) ?? ""
-
-	expect(cl.includes("sel")).toBe(true)
-})
-
-test("Down arrow", async ({ page }) => {
-	await page.keyboard.down("Shift")
-	await page.keyboard.press("ArrowDown")
-
-	const line = page.locator("#pocket-editor .line").nth(1)
-	const cl = (await line.getAttribute("class")) ?? ""
-
-	expect(cl.includes("sel")).toBe(true)
-})
-
-test("Escape removes selection", async ({ page }) => {
-	const line = page.locator("#pocket-editor .line").nth(1)
-
-	await page.keyboard.down("Shift")
-	await page.keyboard.press("ArrowUp")
-
-	expect(((await line.getAttribute("class")) ?? "").includes("sel")).toBe(true)
-	await page.keyboard.press("Escape")
-	expect(((await line.getAttribute("class")) ?? "").includes("sel")).toBe(false)
-})
-
-test("Selects to top", async ({ page }) => {
-	await page.keyboard.down("Shift")
-	await page.keyboard.press("ArrowUp")
-	await page.keyboard.press("ArrowUp")
-	await page.keyboard.press("ArrowUp")
-	await page.keyboard.press("ArrowUp")
-
-	const lines = (await page.locator("#pocket-editor .line.sel").all()).length
-	expect(lines).toBe(2)
-})
-
-test("Selects to bottom", async ({ page }) => {
-	await page.keyboard.down("Shift")
-
-	for (let i = 0; i < 10; i++) {
+	test("With shift + down", async ({ page }) => {
+		await page.keyboard.down("Shift")
 		await page.keyboard.press("ArrowDown")
-	}
+		expect(await line.getAttribute("class")).toBe("line sel")
+	})
 
-	const lines = (await page.locator("#pocket-editor .line.sel").all()).length
-	expect(lines).toBe(9)
+	test("With mouse", async ({ page }) => {
+		const box = await element.boundingBox()
+		const { x, y } = box ?? { x: 0, y: 0 }
+		await page.mouse.move(x + 50, y + 10)
+		await page.mouse.down()
+		await page.mouse.move(x + 50, y + 100)
+
+		const lines = await page.locator("#pocket-editor .line.sel").all()
+		expect(lines.length).toBe(4)
+	})
+
+	test("Max to top", async ({ page }) => {
+		await page.keyboard.down("Shift")
+		await page.keyboard.press("ArrowUp")
+		await page.keyboard.press("ArrowUp")
+		await page.keyboard.press("ArrowUp")
+
+		const lines = (await page.locator("#pocket-editor .line.sel").all()).length
+		expect(lines).toBe(2)
+	})
+
+	test("Max to bottom", async ({ page }) => {
+		await page.keyboard.down("Shift")
+
+		for (let i = 0; i < 10; i++) {
+			await page.keyboard.press("ArrowDown")
+		}
+
+		const lines = (await page.locator("#pocket-editor .line.sel").all()).length
+		expect(lines).toBe(9)
+	})
+
+	test("No shift moves selected line", async ({ page }) => {
+		await page.keyboard.down("Shift")
+		await page.keyboard.press("ArrowDown")
+		await page.keyboard.press("ArrowDown")
+		await page.keyboard.up("Shift")
+		await page.keyboard.press("ArrowDown")
+
+		const line = page.locator("#pocket-editor .line").nth(3)
+		expect(((await line.getAttribute("class")) ?? "").includes("sel")).toBe(true)
+	})
 })
 
-test("No shifts selects one line", async ({ page }) => {
-	await page.keyboard.down("Shift")
-	await page.keyboard.press("ArrowDown")
-	await page.keyboard.press("ArrowDown")
-	await page.keyboard.up("Shift")
-	await page.keyboard.press("ArrowDown")
+test.describe("Action", () => {
+	test.beforeEach(async ({ page }) => {
+		await page.keyboard.down("Shift")
+		await page.keyboard.press("ArrowUp")
+		await page.keyboard.press("ArrowUp")
+		await page.keyboard.up("Shift")
+	})
 
-	const line = page.locator("#pocket-editor .line").nth(3)
-	expect(((await line.getAttribute("class")) ?? "").includes("sel")).toBe(true)
+	test("Click outside removes selection", async ({ page }) => {
+		await page.locator("body").click()
+		expect(await line.getAttribute("class")).toBe("line")
+	})
+
+	test("Escape removes selection", async ({ page }) => {
+		await page.keyboard.press("Escape")
+		expect(await line.getAttribute("class")).toBe("line")
+	})
+
+	test("Backspace removes lines", async ({ page }) => {
+		const linesAmount = (await page.locator(".line").all()).length
+
+		await page.keyboard.press("Backspace")
+
+		expect((await page.locator(".line").all()).length).toBe(linesAmount - 1)
+		expect(await page.locator(".line").first().textContent()).toBe("")
+	})
+
+	test("Typing overwrites lines", async ({ page }) => {
+		await page.keyboard.type("hello world")
+		expect(page.getByText("hello world")).toBeTruthy()
+	})
 })
--- tests/transform.test.ts
+++ tests/transform.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test"
+import { test, expect, Locator } from "@playwright/test"
 
 test.beforeEach(async ({ page }) => {
 	await page.goto("http://localhost:4173")
@@ -81,5 +81,57 @@ test.describe("Press Enter", () => {
 
 		expect(cl?.includes("todo")).toBe(true)
 		expect(line.locator("input")).toBeTruthy()
+	})
+})
+
+test.describe("Type Markdown", () => {
+	let editable: Locator
+	let line: Locator
+
+	test.beforeEach(async ({ page }) => {
+		editable = page.locator("#pocket-editor [contenteditable]").first()
+		line = page.locator("#pocket-editor .line").first()
+		await editable.clear()
+		await page.keyboard.press("Backspace")
+	})
+
+	test("Editable is empty", async ({ page }) => {
+		expect(await editable.textContent()).toBe("")
+	})
+
+	test("to add big heading", async ({ page }) => {
+		await page.keyboard.type("# hello world")
+		expect(await line.getAttribute("class")).toBe("line h1")
+		expect(await editable.textContent()).toBe("hello world")
+	})
+
+	test("to add medium heading", async ({ page }) => {
+		await page.keyboard.type("## hello world")
+		expect(await line.getAttribute("class")).toBe("line h2")
+		expect(await editable.textContent()).toBe("hello world")
+	})
+
+	test("to add small heading", async ({ page }) => {
+		await page.keyboard.type("### hello world")
+		expect(await line.getAttribute("class")).toBe("line h3")
+		expect(await editable.textContent()).toBe("hello world")
+	})
+
+	test("to add list", async ({ page }) => {
+		await page.keyboard.type("- hello world")
+		expect(await line.getAttribute("class")).toBe("line list")
+		expect(await editable.textContent()).toBe("hello world")
+	})
+
+	test("to add todo", async ({ page }) => {
+		await page.keyboard.type("[ ] hello world")
+		expect(await line.getAttribute("class")).toBe("line todo")
+		expect(await editable.textContent()).toBe("hello world")
+	})
+
+	test("to add checked todo", async ({ page }) => {
+		await page.keyboard.type("[x] hello world")
+		expect(await line.getAttribute("class")).toBe("line todo-checked")
+		expect(await editable.textContent()).toBe("hello world")
 	})
 })
